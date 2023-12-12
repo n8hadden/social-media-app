dce620ef155a1a4a5723879450de84d532dfab96
Nathan Hadden <nhadde445@west-mec.org>
2023-12-12 14:25:09 -0700

    First Commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
---
+++ components/navbar.jsx
@@ -0,0 +1,14 @@
+
+
+function Navbar() {
+
+
+    return (
+        <>
+            <nav>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
